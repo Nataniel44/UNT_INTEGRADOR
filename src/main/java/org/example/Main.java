c16448d0d0909de30e563bb5080788b17a307388
Nataniel44 <126529831+Nataniel44@users.noreply.github.com>
2023-04-10 22:21:52 -0300

    otroavance

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,10 @@ import com.opencsv.CSVReader;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 // -------------------------------------PronosticoDeportivo-------------------------------------UTN-------------------------------------ArgentinaPrograma4.0-------------------------------------//
 public class Main {
--- src/main/java/org/example/Ronda.java
+++ src/main/java/org/example/Ronda.java
@@ -1,8 +1,11 @@
 package org.example;
+
 import java.io.FileNotFoundException;
+
 public class Ronda {
     private String nro;
-    private  Partido[] partidos;
+    private Partido[] partidos;
+
     public Ronda(String nro, Partido[] partidos) {
         this.nro = nro;
         this.partidos = partidos;
